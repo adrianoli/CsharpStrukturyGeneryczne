37640095d42a4ae3c797ffa8e56333674c672ca9
Adrian Oliwa <adrianoliwa@o2.pl>
2018-11-02 09:28:28 +0100

    1_5 UnitTests

---
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczneTests/KolejkaKolowaTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using _1_TyptyGeneryczne;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _1_TypyGeneryczneTests
+{
+    [TestClass]
+    public class KolejkaKolowaTest
+    {
+        [TestMethod]
+        public void NowaKolejkaJestPusta()
+        {
+            var nowaKolejka = new KolejkaKolowa();
+            Assert.IsTrue(nowaKolejka.JestPusty);
+        }
+
+        [TestMethod]
+        public void KolejkaTrzyElementowaJestPelnaPoTrzechZapisach()
+        {
+            var nowaKolejka = new KolejkaKolowa(pojemnosc: 3);
+            nowaKolejka.Zapisz(3.4);
+            nowaKolejka.Zapisz(8.2);
+            nowaKolejka.Zapisz(7);
+
+            Assert.IsTrue(nowaKolejka.JestPelny);
+        }
+
+        [TestMethod]
+        public void PierwszyWchodziPierwszyWychodzi()
+        {
+            var nowaKolejka = new KolejkaKolowa(pojemnosc: 3);
+            var wartosc1 = 3.5;
+            var wartosc2 = 4.5;
+
+            nowaKolejka.Zapisz(wartosc1);
+            nowaKolejka.Zapisz(wartosc2);
+
+            Assert.AreEqual(wartosc1, nowaKolejka.Czytaj());
+            Assert.AreEqual(wartosc2, nowaKolejka.Czytaj());
+            Assert.IsTrue(nowaKolejka.JestPusty);
+        }
+
+        [TestMethod]
+        public void NadpisujeGdyJestWiekszaNizPojemnosc()
+        {
+            var nowaKolejka = new KolejkaKolowa(pojemnosc: 3);
+            var wartosci = new[] { 1.2, 2.4, 4.2, 5.3, 5.6, 8.3 };
+
+            foreach(var wartosc in wartosci)
+            {
+                nowaKolejka.Zapisz(wartosc);
+            }
+
+            Assert.IsTrue(nowaKolejka.JestPelny);
+            Assert.AreEqual(wartosci[3], nowaKolejka.Czytaj());
+            Assert.AreEqual(wartosci[4], nowaKolejka.Czytaj());
+            Assert.AreEqual(wartosci[5], nowaKolejka.Czytaj());
+            Assert.IsTrue(nowaKolejka.JestPusty);
+        }
+    }
+}
---
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("1_TypyGeneryczneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1_TypyGeneryczneTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4421580d-7299-48d3-87fe-ed75ea2f7749")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
