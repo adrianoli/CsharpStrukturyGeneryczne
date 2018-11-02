9ed978918ef4fabb39014162560b30f21d2c63e3
Adrian Oliwa <adrianoliwa@o2.pl>
2018-11-02 13:34:52 +0100

    Refactoring

--- CsharpStrukturyGeneryczne/1_TyptyGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/1_TyptyGeneryczne/Program.cs
@@ -7,13 +7,30 @@ namespace _1_TyptyGeneryczne
         static void Main(string[] args)
         {
             var kolejka = new KolejkaKolowa<double>(pojemnosc: 3);
+            WczytajDane(kolejka);
+            WyswietlWynik(kolejka);
 
-            while(true)
+            Console.ReadKey();
+        }
+
+        private static void WyswietlWynik(KolejkaKolowa<double> kolejka)
+        {
+            Console.WriteLine("W naszej kolejce jest: ");
+
+            while (!kolejka.JestPusty)
+            {
+                Console.WriteLine("\t\t" + kolejka.Czytaj());
+            }
+        }
+
+        private static void WczytajDane(KolejkaKolowa<double> kolejka)
+        {
+            while (true)
             {
                 var wartosc = 0.0;
                 var wartoscWejsciowa = Console.ReadLine();
 
-                if(double.TryParse(wartoscWejsciowa, out wartosc))
+                if (double.TryParse(wartoscWejsciowa, out wartosc))
                 {
                     kolejka.Zapisz(wartosc);
                     continue;
@@ -21,15 +38,6 @@ namespace _1_TyptyGeneryczne
 
                 break;
             }
-
-            Console.WriteLine("W naszej kolejce jest: ");
-
-            while(!kolejka.JestPusty)
-            {
-                Console.WriteLine("\t\t" + kolejka.Czytaj());
-            }
-
-            Console.ReadKey();
         }
     }
 }
